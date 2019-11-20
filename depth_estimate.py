5b60ef42129978878a1c817051479f41cca6af16
wzy810103882 <zw2585@columbia.edu>
2019-11-20 16:34:48 -0500

    sync

---
+++ depth_estimate.py
@@ -0,0 +1,56 @@
+import os
+import glob
+import argparse
+import matplotlib
+
+# Keras / TensorFlow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
+from keras.models import load_model
+from layers import BilinearUpSampling2D
+from utils import predict, load_images, display_images
+from matplotlib import pyplot as plt
+
+# Argument Parser
+parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
+parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
+#parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
+parser.add_argument('--input', default='/work/NYUv2/nyu_test_rgb/*.png', type=str, help='Input filename or folder.')  # edit input directory
+
+
+args = parser.parse_args()
+
+# Custom object needed for inference and training
+custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
+
+print('Loading model...')
+
+# Load model into GPU / CPU
+model = load_model(args.model, custom_objects=custom_objects, compile=False)
+
+print('\nModel loaded ({0}).'.format(args.model))
+
+# Input images
+
+names = sorted(glob.glob(args.input))
+inputs = load_images(names)
+print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
+
+# Compute results
+outputs = predict(model, inputs)
+
+for i in range(outputs.shape[0]):
+    one_sample = outputs[i][:,:,0]
+    print(one_sample.shape)
+    print(one_sample)
+    break
+
+
+#matplotlib problem on ubuntu terminal fix
+#matplotlib.use('TkAgg')   
+
+# Display results
+#viz = display_images(outputs.copy(), inputs.copy())
+#plt.figure(figsize=(10,5))
+#plt.imshow(viz)
+#plt.savefig('test.png')
+#plt.show()
