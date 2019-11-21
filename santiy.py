1db6f30639a15ccd41f208436677bb1751cdd48e
wzy810103882 <zw2585@columbia.edu>
2019-11-20 19:44:30 -0500

    Create santiy.py

---
+++ santiy.py
@@ -0,0 +1,12 @@
+import glob
+from utils import predict, load_images, display_images, scale_up
+import numpy as np 
+
+
+names = sorted(glob.glob('/work/NYUv2/nyu_test_rgb/*.png'))
+print(names)
+inputs = load_images(names[:5])
+print(inputs.shape)
+print(inputs[1,50:100,50:100,0])
+print(np.amax(inputs[1,:,:,0]))
+print(np.amin(inputs[1,:,:,0]))
