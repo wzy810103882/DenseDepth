92a7311a887fa1398a9152b7b6a19fd6e33a8283
wzy810103882 <zw2585@columbia.edu>
2019-11-20 19:46:01 -0500

    Update santiy.py

--- santiy.py
+++ santiy.py
@@ -4,9 +4,12 @@ import numpy as np
 
 
 names = sorted(glob.glob('/work/NYUv2/nyu_test_rgb/*.png'))
-print(names)
+print(names[:5])
 inputs = load_images(names[:5])
 print(inputs.shape)
 print(inputs[1,50:100,50:100,0])
+print(inputs[0,50:100,50:100,0])
+print(inputs[0,0:50,0:50,0])
+
 print(np.amax(inputs[1,:,:,0]))
 print(np.amin(inputs[1,:,:,0]))
